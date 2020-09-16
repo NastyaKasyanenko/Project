ff934490105287e5660dbb08aa19e78a28c4fcf1
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-09-16 21:34:30 +0300

    test code 2

--- src/main/java/ru/ssau/tk/_Anastasia_/_PROJECT_/Zadanie.java
+++ src/main/java/ru/ssau/tk/_Anastasia_/_PROJECT_/Zadanie.java
@@ -3,11 +3,20 @@ package ru.ssau.tk._Anastasia_._PROJECT_;
 import java.util.Scanner;
 public class Zadanie {
     public static void main(String[] args) {
-        int x;
         Scanner in = new Scanner(System.in);
-        x = in.nextInt();
-        x = x * x;
-        System.out.println(x);
+        System.out.println("Введите 2 любых целых числа: ");
+        int x = in.nextInt();//считывает первое число и присваивает значение в x
+        int y = in.nextInt();//считывает второе число и присваивает значение в y
+        int i = in.nextInt ();
+        if ( i==1) {
+            System.out.println (x + y);
+        }
+        else if (i==2) {
+            System.out.println (x - y);
+        }
+        else if (i==3) {
+            System.out.println (x * y);
+        }
 
     }
 }
