7157be2ccd7bf4698a4fed400cd946c0dc7fed97
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-09-20 22:27:17 +0300

    Create Person.java

---
+++ src/main/java/ru/ssau/tk/Practice/Person/Person.java
@@ -0,0 +1,39 @@
+package ru.ssau.tk.Practice.Person;
+
+public class Person {
+
+        private String firstName;
+        private String lastName;
+        private int passportId;
+
+        public Person(String firstName, String lastName, int passportId){
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.passportId = passportId;
+        }
+        public String getfirstName () {
+            System.out.println(firstName);
+            return firstName;
+        }
+
+        public void setfirstName (String firstName){
+            this.firstName = firstName;
+        }
+
+        public String getlastName () {
+            return lastName;
+        }
+
+        public void setlastName (String lastName){
+            this.lastName = lastName;
+        }
+
+        public int getpassportId () {
+            return passportId;
+        }
+
+        public void setpassportId ( int passportId){
+            this.passportId = passportId;
+        }
+    }
+
