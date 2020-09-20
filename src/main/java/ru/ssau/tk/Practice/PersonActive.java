0766d20ebe6cb4ad9ad73412c2cc8d476b44e97e
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-09-20 22:25:44 +0300

    Finally some freaking good code

---
+++ src/main/java/ru/ssau/tk/Practice/PersonActive.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.Practice;
+import ru.ssau.tk.Practice.Person.*;
+import ru.ssau.tk.Practice.Person.Person;
+
+public class PersonActive {
+
+    public static void main(String[] s){
+        Person person = new Person("Ivan", "Pelmenikov", 228);
+        person.getfirstName();
+    }
+
+}
