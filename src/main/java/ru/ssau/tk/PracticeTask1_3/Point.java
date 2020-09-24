997dfc3facec9b6359874e7a1ef3ff5222c68328
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-09-24 09:21:40 +0300

    Task 1_3

---
+++ src/main/java/ru/ssau/tk/PracticeTask1_3/Point.java
@@ -0,0 +1,16 @@
+package ru.ssau.tk.PracticeTask1_3;
+
+public class Point {
+    public final double x;
+    public final double y;
+    public final double z;
+
+    public Point(double x, double y, double z){
+        this.x=x;
+        this.y=y;
+        this.z=z;
+    }
+    Point point1 = new Point(1,2,3);
+    Point point2 = new Point(4, 5, 6);
+    Point point3 = new Point(7, 8, 9);
+}
