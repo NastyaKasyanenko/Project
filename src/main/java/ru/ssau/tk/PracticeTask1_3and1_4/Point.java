dfefcce94e3adb0ca59140ab0cb7a2ba0578c948
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-09-30 19:16:42 +0300

    добавление метода length() (задание 1_6)

--- src/main/java/ru/ssau/tk/PracticeTask1_3and1_4/Point.java
+++ src/main/java/ru/ssau/tk/PracticeTask1_3and1_4/Point.java
@@ -5,12 +5,19 @@ public class Point {
     public final double y;
     public final double z;
 
-    public Point(double x, double y, double z){
-        this.x=x;
-        this.y=y;
-        this.z=z;
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
-    Point point1 = new Point(1,2,3);
-    Point point2 = new Point(4, 5, 6);
-    Point point3 = new Point(7, 8, 9);
+
+    double length() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public static void main(String[] args) {
+        Point point1 = new Point(1, 2, 3);
+        Point point2 = new Point(4, 5, 6);
+        Point point3 = new Point(7, 8, 9);
+    }
 }
