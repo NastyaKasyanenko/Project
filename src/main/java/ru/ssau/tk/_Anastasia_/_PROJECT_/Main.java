0efb1de7b65c4d91689c33f0ed624d0900659823
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-10-05 20:45:00 +0300

    Тест сканера и equals

---
+++ src/main/java/ru/ssau/tk/_Anastasia_/_PROJECT_/Main.java
@@ -0,0 +1,13 @@
+package ru.ssau.tk._Anastasia_._PROJECT_;
+import java.util.Scanner;
+public class Main {
+
+        public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+        String s1 = in.nextLine();
+        String s2 = new String("Hello!");
+            System.out.println(s1.equals(s2));
+        }
+    }
+
