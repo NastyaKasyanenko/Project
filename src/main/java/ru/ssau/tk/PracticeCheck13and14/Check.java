5b9846044236e95e8b90b2e3f5f63c8eaa45f6b4
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-10-06 21:45:53 +0300

    Добавлены комментарии к заданию 1_14

--- src/main/java/ru/ssau/tk/PracticeCheck13and14/Check.java
+++ src/main/java/ru/ssau/tk/PracticeCheck13and14/Check.java
@@ -2,15 +2,16 @@ package ru.ssau.tk.PracticeCheck13and14;
 import ru.ssau.tk.PracticeTask1_2.Person;
 public class Check {
     public static void main(String[] args) {
-        Person person = new Person();
+        Person person = new Person();/*Создала объект в среде main*/
         person.setFirstName("Arkadiy");
         checkAnotherPerson(person);
-        System.out.println(person.getfirstName());
+        System.out.println(person.getfirstName());/*Вывела объект в main*/
     }
 
     private static void checkAnotherPerson(Person person) {
+        /*объект не используется, как есть новый инициализированный объект */
         person = new Person();
         person.setFirstName("Ignat");
-        System.out.println(person.getfirstName());
+        System.out.println(person.getfirstName());/*Значение*/
     }
 }
