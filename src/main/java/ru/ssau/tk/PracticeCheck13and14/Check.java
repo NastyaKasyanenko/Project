289ca68a5341050495b666118d7a17cb8edfd11f
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-10-06 21:44:04 +0300

    Задание 1_14

--- src/main/java/ru/ssau/tk/PracticeCheck13and14/Check.java
+++ src/main/java/ru/ssau/tk/PracticeCheck13and14/Check.java
@@ -4,20 +4,13 @@ public class Check {
     public static void main(String[] args) {
         Person person = new Person();
         person.setFirstName("Arkadiy");
-        int number = 5;
-        checkInt(number);
-        checkPerson(person);
-        System.out.println(number);/*3) Выводится 5, т.к в chekInt() поменялась копия переменной number.*/
-        System.out.println(person.getfirstName()); /*4) Выводится Oleg т.к объект ссылается на  данные, полученные
-        из-за передачи копии адреса объекта.*/
+        checkAnotherPerson(person);
+        System.out.println(person.getfirstName());
     }
 
-    private static void checkInt(int number) {
-        number = 10;/*1) Выводится 10 т.к  копия переменной int number ссылается  на значение 10.*/
-        System.out.println(number);
-    }
-    private static void checkPerson(Person person) {
-        person.setFirstName("Oleg");/*2) Выводится Oleg  т.к передаётся копия ссылки на объект и указывает на тот же объект.*/
+    private static void checkAnotherPerson(Person person) {
+        person = new Person();
+        person.setFirstName("Ignat");
         System.out.println(person.getfirstName());
     }
 }
