4b98b4dc3694792559030bb871ba0cfc1da7fc57
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-10-14 09:10:24 +0300

    разобралась в работе equals

--- src/main/java/ru/ssau/tk/_Anastasia_/_PROJECT_/Area.java
+++ src/main/java/ru/ssau/tk/_Anastasia_/_PROJECT_/Area.java
@@ -25,6 +25,7 @@ public class Area {
                 double area = (num1 * num2);
                 System.out.println("Площадь треугольника равна: " + area);
                 i++;
+
             } else if (choice.equals(choice3)) {
                 System.out.println("Введите радиус круга: ");
                 double radius = in.nextDouble();
