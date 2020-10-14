b8f01c2e9b864aafbe2519479a5b543bbe9a15c4
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-10-14 08:51:21 +0300

    еще один тест метода equals() (вычисление площадей)

--- src/main/java/ru/ssau/tk/_Anastasia_/_PROJECT_/Area.java
+++ src/main/java/ru/ssau/tk/_Anastasia_/_PROJECT_/Area.java
@@ -1,19 +1,37 @@
 package ru.ssau.tk._Anastasia_._PROJECT_;
+
 import java.util.Scanner;
+
 public class Area {
     public static void main(String args[]) {
-        String choice, choice1 = "triangle" , choice2 = "rectangle", choice3 = "circle";
-            Scanner scanner = new Scanner(System.in);
-        System.out.println("Please write your choice");
-        choice = in.nextLine();
-        System.out.println("Your name is:");
-        System.out.println(choice);
-            System.out.println("Введите ширину треугольника: ");
-            double base = scanner.nextDouble();
-
-            System.out.println("Введите высоту треугольника: ");
-            double height = scanner.nextDouble();
-            double area = (base* height)/2;
-            System.out.println("Площадь треугольника равна: " + area);
+        String choice, choice1 = "triangle", choice2 = "rectangle", choice3 = "circle";
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < 1; ) {
+            System.out.println("Please write your choice");
+            choice = in.nextLine();
+            if (choice.equals(choice1)) {
+                System.out.println("Введите ширину треугольника: ");
+                double base = in.nextDouble();
+                System.out.println("Введите высоту треугольника: ");
+                double height = in.nextDouble();
+                double area = (base * height) / 2;
+                System.out.println("Площадь треугольника равна: " + area);
+                i++;
+            } else if (choice.equals(choice2)) {
+                System.out.println("Введите длину прямоугольника: ");
+                double num1 = in.nextDouble();
+                System.out.println("Введите ширину прямоугольника: ");
+                double num2 = in.nextDouble();
+                double area = (num1 * num2);
+                System.out.println("Площадь треугольника равна: " + area);
+                i++;
+            } else if (choice.equals(choice3)) {
+                System.out.println("Введите радиус круга: ");
+                double radius = in.nextDouble();
+                double area = (radius * radius * Math.PI);
+                System.out.println("Площадь треугольника равна: " + area);
+                i++;
+            }
         }
+    }
 }
