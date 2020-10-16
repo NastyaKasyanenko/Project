7067025f3fa73caf27a8ef8d067a84d49ad3a87b
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-10-16 14:48:33 +0300

    поправила тест для IdenticalOperation

--- src/test/java/ru/ssau/tk/PracticeTask1_15/IdenticalOperationTest.java
+++ src/test/java/ru/ssau/tk/PracticeTask1_15/IdenticalOperationTest.java
@@ -8,6 +8,7 @@ public class IdenticalOperationTest {
     private static final double DELTA  = 0.00001;
     private static final double NEGATIVE_INFINITY = 0.00001;
     private static final double POSITIVE_INFINITY  = 0.00001;
+    private static final double NaN  = 0.00001;
     Operation Identical = new IdenticalOperation();
 
     @Test
@@ -15,5 +16,11 @@ public class IdenticalOperationTest {
         assertEquals(Identical.apply(300), 300, DELTA);
         assertEquals(Identical.apply(NEGATIVE_INFINITY), NEGATIVE_INFINITY, DELTA);
         assertEquals(Identical.apply(POSITIVE_INFINITY), POSITIVE_INFINITY, DELTA);
+    }
+    @Test
+    public void testApplyTriple() {
+        assertEquals(Identical.apply(Double.NaN),Double.NaN,DELTA);
+        assertEquals(Identical.apply(Double.POSITIVE_INFINITY), Double.POSITIVE_INFINITY, DELTA);
+        assertEquals(Identical.apply(Double.NEGATIVE_INFINITY), Double.NEGATIVE_INFINITY, DELTA);
     }
 }
