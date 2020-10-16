18db2d208dda60ceff7897212ba4ea15180f8553
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-10-16 14:01:02 +0300

    создан класс TangentOperation, наследник Operation

---
+++ src/main/java/ru/ssau/tk/PracticeTask1_15/TangentOperation.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.PracticeTask1_15;
+
+public class TangentOperation extends Operation{
+    @Override
+    double apply(double number) {
+        return Math.tan(number);
+    }
+}
