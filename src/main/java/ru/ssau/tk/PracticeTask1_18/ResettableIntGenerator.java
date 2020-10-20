2cc448cd2b318f09bba1342c444728b6b9189afa
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-10-20 16:09:04 +0300

    создан класс ResettableIntGenerator

---
+++ src/main/java/ru/ssau/tk/PracticeTask1_18/ResettableIntGenerator.java
@@ -0,0 +1,20 @@
+package ru.ssau.tk.PracticeTask1_18;
+
+public class ResettableIntGenerator implements Resettable, IntGenerator {
+    private int x = 0;
+    String name = "Absent";
+
+    @Override
+    public int nextInt() {
+        return x++;
+    }
+
+    @Override
+    public void reset() {
+        x = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
