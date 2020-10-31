4150f2fd484b7e7e66ca9b5a2a46558be737375a
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-10-31 15:38:17 +0300

    задание 2_14

---
+++ src/main/java/ru/ssau/tk/PracticeTask2_14/Massiv11.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.PracticeTask2_14;
+
+public class Massiv11 {
+    static void massiv(int[] massiv) {
+        for (int i = 0; i < massiv.length; i++) {
+            massiv[i] = -((Integer) massiv[i]);
+
+        }
+    }
+}
+
+
