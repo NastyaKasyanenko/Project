cdec961337dc7872357eb9ffc7b9adee9d340a77
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-01 18:50:16 +0300

    тест для задания 2_15...ошиблась

--- src/test/java/ru/ssau/tk/PracticeTask2_15/Massiv12Test.java
+++ src/test/java/ru/ssau/tk/PracticeTask2_15/Massiv12Test.java
@@ -8,9 +8,9 @@ public class Massiv12Test {
 
     @Test
     public void testMassiv() {
-        int[] massiv = {1, 3, 5, 7,9};
-        assertEquals(Massiv12.massiv(massiv,5),true);
-        assertEquals(Massiv12.massiv(massiv,2),false);
-        assertNotEquals(Massiv12.massiv(massiv,8),true);
+        int[] massiv = {1, 3, 5, 7, 9};
+        assertEquals(Massiv12.massiv(massiv, 5), true);
+        assertEquals(Massiv12.massiv(massiv, 2), false);
+        assertNotEquals(Massiv12.massiv(massiv, 8), true);
     }
 }
