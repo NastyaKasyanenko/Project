3b7d59d93246304029f1dfc4b253a9d43f387402
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-01 18:47:59 +0300

    тест для задания 2_14

---
+++ src/test/java/ru/ssau/tk/PracticeTask2_15/Massiv12Test.java
@@ -0,0 +1,16 @@
+package ru.ssau.tk.PracticeTask2_15;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class Massiv12Test {
+
+    @Test
+    public void testMassiv() {
+        int[] massiv = {1, 3, 5, 7,9};
+        assertEquals(Massiv12.massiv(massiv,5),true);
+        assertEquals(Massiv12.massiv(massiv,2),false);
+        assertNotEquals(Massiv12.massiv(massiv,8),true);
+    }
+}
