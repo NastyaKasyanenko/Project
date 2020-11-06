30ab10b85b179d4a71d40f48ec9be5e345e43405
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-06 12:21:59 +0300

    Ctrl + Alt + L

--- src/test/java/ru/ssau/tk/PracticeTask2_21/Massiv18Test.java
+++ src/test/java/ru/ssau/tk/PracticeTask2_21/Massiv18Test.java
@@ -9,8 +9,8 @@ public class Massiv18Test {
     @Test
     public void testNumber() {
         int[] array = new int[]{1, 3, 5, 5, 7};
-        assertEquals(Massiv18.Number(array),5);
+        assertEquals(Massiv18.Number(array), 5);
         int[] temp = new int[]{0, 0, 1, 2, 3};
-        assertEquals(Massiv18.Number(temp),0);
+        assertEquals(Massiv18.Number(temp), 0);
     }
 }
