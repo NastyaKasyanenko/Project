c75aa0f3ab8d62bb92b351817fe61e2e099c1270
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-06 12:18:21 +0300

    тест для задания 2_21

---
+++ src/test/java/ru/ssau/tk/PracticeTask2_21/Massiv18Test.java
@@ -0,0 +1,16 @@
+package ru.ssau.tk.PracticeTask2_21;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class Massiv18Test {
+
+    @Test
+    public void testNumber() {
+        int[] array = new int[]{1, 3, 5, 5, 7};
+        assertEquals(Massiv18.Number(array),5);
+        int[] temp = new int[]{0, 0, 1, 2, 3};
+        assertEquals(Massiv18.Number(temp),0);
+    }
+}
