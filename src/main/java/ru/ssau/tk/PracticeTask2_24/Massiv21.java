f5a6ef7ef61131936e3da742e046b5b18b3b9209
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-11 19:35:41 +0300

    задание 2_24

---
+++ src/main/java/ru/ssau/tk/PracticeTask2_24/Massiv21.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.PracticeTask2_24;
+
+public class Massiv21 {
+    static void sdvig(int[] massiv) {
+        for (int i = 0; i < massiv.length; i++) {
+            massiv[i] = ~massiv[i];
+        }
+    }
+}
