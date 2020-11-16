a3656d7d7a92e99924c0f90fb3ec2b49d3b50cc1
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-16 22:04:01 +0300

    задание 2_30

---
+++ src/main/java/ru/ssau/tk/PracticeTask2_30/Massiv27.java
@@ -0,0 +1,16 @@
+package ru.ssau.tk.PracticeTask2_30;
+
+public class Massiv27 {
+    static int[][] Vector(int size) {
+        int[][] arr = new int[size][];
+        int k = 1;
+        for (int i = 0; i < size; i++) {
+            arr[i] = new int[size - i];
+            for (int j = 0; j < size - i; j++) {
+                arr[i][j] = k;
+                k++;
+            }
+        }
+        return arr;
+    }
+}
