083642313d49d0bf8dbd034628f27f49f776c25a
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-17 21:11:12 +0300

    тест для задания 2_33

---
+++ src/test/java/ru/ssau/tk/PracticeTask2_33/Massiv29Test.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.PracticeTask2_33;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class Massiv29Test {
+
+    @Test
+    public void testArrSting() {
+        String[] array = new String[]{"I","am","happy"};
+        Massiv29.arrSting(array);
+    }
+}
