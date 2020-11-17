78726a058ac4fb34704f6a93d48ac9ee42cad794
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-17 21:06:18 +0300

    задание 2_33

---
+++ src/main/java/ru/ssau/tk/PracticeTask2_33/Massiv29.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.PracticeTask2_33;
+
+public class Massiv29 {
+    static void arrSting(String[] array) {
+        for (String i : array) {
+            System.out.println(i);
+        }
+    }
+}
