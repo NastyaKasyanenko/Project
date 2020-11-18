ca2d987a0e84e73894f60f711ded05dc33972e56
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-18 19:36:36 +0300

    задание 3_1

---
+++ src/main/java/ru/ssau/tk/PracticeTask3_1/Stroki.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.PracticeTask3_1;
+
+public class Stroki {
+    public void setString(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+    }
+}
