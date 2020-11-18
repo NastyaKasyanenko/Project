666df14cc8d9cd05f34d697208b05dfe1649b849
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-18 19:14:48 +0300

    задание 3_2

---
+++ src/main/java/ru/ssau/tk/PracticeTask3_2/Stroki1.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.PracticeTask3_2;
+
+public class Stroki1 {
+    public void str(String str){
+        byte[] bytes = str.getBytes();
+        for (byte byt:bytes) {
+            System.out.println(byt);
+        }
+    }
+}
