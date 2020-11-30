4a80902db917cf993a6759c5adf31d800ab41e5a
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-30 20:11:06 +0300

    задание 3_13

---
+++ src/main/java/ru/ssau/tk/PracticeTask3_13/Stroki12.java
@@ -0,0 +1,16 @@
+package ru.ssau.tk.PracticeTask3_13;
+
+public class Stroki12 {
+    static String string(String str, int from, int to) {
+        if (from < 0) {
+            from = 0;
+        }
+        if (to > str.length()) {
+            to = str.length();
+        }
+        if (from == to) {
+            return "";
+        }
+        return str.substring(from, to);
+    }
+}
