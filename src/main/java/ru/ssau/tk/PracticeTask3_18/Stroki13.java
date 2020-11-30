8b9d7e509c6bc5d28bea34e1bf9bdf79bbb907bb
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-11-30 21:14:51 +0300

    задание 3_18

---
+++ src/main/java/ru/ssau/tk/PracticeTask3_18/Stroki13.java
@@ -0,0 +1,8 @@
+package ru.ssau.tk.PracticeTask3_18;
+
+public class Stroki13 {
+    public static void getDescription(Object obj){
+        System.out.println("Описание объекта: ");
+        System.out.println(obj);
+    }
+}
