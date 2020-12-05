fbed31b2bbcbfc13b1ce1869b8629a28be45c4ff
NastyaKasyanenko <71152435+NastyaKasyanenko@users.noreply.github.com>
2020-12-05 15:03:00 +0300

    3_23

---
+++ src/main/java/ru/ssau/tk/PracticeTask3_23/Stroki18.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.PracticeTask3_23;
+
+import java.nio.charset.Charset;
+
+public class Stroki18 {
+    public static void main(String[] args) {
+        System.out.println(Charset.defaultCharset());
+    }
+}
